6bc80fec41e994ab1c4085cf66872e48347919f2
Nick Tunac <dominick.tunac@gmail.com>
2016-08-13 19:43:21 +0800

    Create Initial Debugging Environment

---
+++ src/main/java/com/ideyatech/debuggit/MainClass.java
@@ -0,0 +1,41 @@
+package com.ideyatech.debuggit;
+
+public class MainClass {
+
+	
+	public static void main(String[] args) {
+		
+		int someValue = 10;
+		
+		int result = someMethod(someValue);
+		
+		System.out.println(result);
+		
+		if (result == 10) {
+			//Do SomethingNiceMethod()
+		} else {
+			//Do SomethingBadMethod()
+		}
+		
+		
+		
+		MathHelper mh = new MathHelper();
+		
+		int addValue = mh.add(10, 10);
+		
+		
+		
+	}
+	
+	public static int someMethod(int someValue) {
+		
+		//some calculation
+		
+		someValue += 10;
+		 
+		return 10;
+		
+	}
+	
+	
+}
---
+++ src/main/java/com/ideyatech/debuggit/MathHelper.java
@@ -0,0 +1,13 @@
+package com.ideyatech.debuggit;
+
+public class MathHelper {
+	
+	public int add(int a, int b) {
+		
+		return a + b;
+		
+	}
+	
+	//TODO Create other math methods
+
+}
